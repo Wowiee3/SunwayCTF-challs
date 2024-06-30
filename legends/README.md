251ae61b141b4dbfaa8e457a7dae3ea541ed7b2b
Robbin Ooi Zhen Heng <121353711+warlocksmurf@users.noreply.github.com>
2024-06-30 16:49:09 +0800

    Delete legends directory

--- legends/README.md
+++
@@ -1,4 +0,0 @@
-## Legends
-My waifu image was encrypted by a ransomware, please help me recover it...
-
-Flag: `sunctf{lnk_or_zelda?}`
--- legends/cobalt.ps1
+++
@@ -1,6 +0,0 @@
-$WshShell = New-Object -ComObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("C:\Users\warlocksmurf\legends.lnk")
-$Shortcut.TargetPath = "powershell.exe"
-$Shortcut.Arguments = "-window hidden -noni -enc JGJ5dGVzPVtTeXN0ZW0uSU8uRmlsZV06OlJlYWRBbGxCeXRlcygnQzpcVXNlcnNcd2FybG9ja3NtdXJmXGZsYWcuanBnJyk7Zm9yKCRpPTA7JGkgLWx0ICRieXRlcy5MZW5ndGg7JGkrKyl7JGJ5dGVzWyRpXT0kYnl0ZXNbJGldLWJ4b3IgMzV9O1tTeXN0ZW0uSU8uRmlsZV06OldyaXRlQWxsQnl0ZXMoJ0M6XFVzZXJzXHdhcmxvY2tzbXVyZlx6ZWxkYS5qcGcnLCRieXRlcyk="
-
-$Shortcut.Save()

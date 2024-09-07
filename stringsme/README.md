47ee84f7024338732cd48e584897408154cb73e1
Wowiee3 <estherlesliebala@gmail.com>
2024-09-07 15:34:48 +0800

    added rev chall

---
+++ stringsme/README.md
@@ -0,0 +1,1 @@
+ez rev chall. just strings the stringsme file. source code not to be included in final chall, but is here for reference.
---
+++ stringsme/string.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    printf("I am an innocent program. Have you heard about the strings command?\n");
+    char *p = "sunctf{us3str1ng5}";
+
+}

75a6115d56fe492d01d78fd8bb2f53c68483f35d
shen <77434180+chuajianshen@users.noreply.github.com>
2024-09-19 12:10:58 +0800

    Delete web_nasi_kandar directory

--- web_nasi_kandar/sql/db.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users(
-  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  username VARCHAR(256) NOT NULL,
-  password VARCHAR(256) NOT NULL
-);
-
-INSERT INTO users (username, password) VALUES ('admin', 'admin');

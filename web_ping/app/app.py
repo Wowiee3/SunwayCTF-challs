af5e86aef07dc0d0ad827c619ab913fa5eb34d44
shen <77434180+chuajianshen@users.noreply.github.com>
2024-09-19 12:46:15 +0800

    Update app.py

--- web_ping/app/app.py
+++ web_ping/app/app.py
@@ -30,4 +30,4 @@ def ping():
         return render_template('index.html', result=error_msg)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002)
+    app.run(host='0.0.0.0', port=5004)
